9f7b7d374033f7a05d182b6306b8d2d78b440f0d
BCGen <bcgenever7@gmail.com>
2018-11-09 13:12:00 +0800

    update

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use App\Services\Twitter;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects.index', compact('projects'));
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => ['required', 'min:3'],
+            'description' => ['required', 'min:3'],
+        ]);
+
+        Project::create($attributes);
+
+        return redirect('/projects');
+    }
+
+    public function edit(Project $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
+    public function show(Project $project, Twitter $twitter)
+    {
+        dd($twitter);
+
+        return view('projects.show', compact('project'));
+    }
+
+    public function update(Project $project)
+    {
+        $project->update(request(['title', 'description']));
+        return redirect('/projects');
+    }
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return redirect('projects');
+    }
+}
